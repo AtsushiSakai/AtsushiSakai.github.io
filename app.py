ea7a27cfb9952a1afee4f84d1fd50ff0b50b757a
AtsushiSakai <asakai.amsl+github@gmail.com>
2015-11-28 21:01:51 +0900

    add

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def index():
 
 if __name__ == '__main__':
     if os.getenv("HEROKU")==None:       
-        run(host="localhost", port=(os.environ.get("PORT",5100)), debug=True, reloader=True)
+        #run(host="localhost", port=(os.environ.get("PORT",5100)), debug=True, reloader=True)
+        run(host="192.168.3.6", port=(os.environ.get("PORT",5100)), debug=True, reloader=True)
     else:
         run(host="0.0.0.0", port=(os.environ.get("PORT",5200)), debug=True, reloader=True)
