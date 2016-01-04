ab5ff10b408d878cc7329b87060f02144bf1c8e3
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-01-04 18:30:47 +0900

    update

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ def index():
 
 if __name__ == '__main__':
     if os.getenv("HEROKU")==None:       
+        # local
         #run(host="localhost", port=(os.environ.get("PORT",5100)), debug=True, reloader=True)
+        # githubpages
         run(host="192.168.3.6", port=(os.environ.get("PORT",5100)), debug=True, reloader=True)
     else:
         run(host="0.0.0.0", port=(os.environ.get("PORT",5200)), debug=True, reloader=True)
